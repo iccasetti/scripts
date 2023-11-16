7044f6768512652012220236916323322b90a8d8
iccasetti <150969799+iccasetti@users.noreply.github.com>
2023-11-16 15:24:59 +0100

    Update web-boot.bat

--- web-boot.bat
+++ web-boot.bat
@@ -1,4 +1,5 @@
 SETLOCAL
 REM choco install -y scratch
 winget install -h --disable-interactivity "Chimpa Agent"
+REM admin@chimpa.private -> https://cloud.chimpa.eu/iccasetti/api/latest/mdm/windows/discovery_windows
 ECHO %USERNAME% > %TEMP%\WEB-BOOT.LOG
