30990383341328b8ac5645ccda08f2e4d0a3d42f
iccasetti <150969799+iccasetti@users.noreply.github.com>
2023-11-20 13:26:08 +0100

    Update web-boot.bat

--- web-boot.bat
+++ web-boot.bat
@@ -1,8 +1,9 @@
 SETLOCAL
-choco install -y autoit
-sc config w32time start= auto
+choco install -y autoit &
+sc config w32time start= auto &
 net start w32time
-w32tm /resync & 
+w32tm /config /manualpeerlist:"tempo.ien.it"
+w32tm /resync /nowait & 
 REM winget install -h --disable-interactivity "Chimpa Agent"
 REM admin@chimpa.private -> https://cloud.chimpa.eu/iccasetti/api/latest/mdm/windows/discovery_windows
 ECHO %USERNAME% > %TEMP%\WEB-BOOT.LOG
