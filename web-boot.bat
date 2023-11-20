de37045910f038e88e726c131122c968b5897edf
iccasetti <150969799+iccasetti@users.noreply.github.com>
2023-11-20 12:29:28 +0100

    Update web-boot.bat

--- web-boot.bat
+++ web-boot.bat
@@ -1,5 +1,8 @@
 SETLOCAL
 choco install -y autoit
-winget install -h --disable-interactivity "Chimpa Agent"
+sc config w32time start= auto
+net start w32time
+w32tm /resync & 
+REM winget install -h --disable-interactivity "Chimpa Agent"
 REM admin@chimpa.private -> https://cloud.chimpa.eu/iccasetti/api/latest/mdm/windows/discovery_windows
 ECHO %USERNAME% > %TEMP%\WEB-BOOT.LOG
