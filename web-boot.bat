3016ada827bf9ad84aef6b65a734614103600049
iccasetti <150969799+iccasetti@users.noreply.github.com>
2023-11-23 11:30:11 +0100

    Update web-boot.bat with cura settings

--- web-boot.bat
+++ web-boot.bat
@@ -8,6 +8,9 @@ choco install -y autoit &
 choco install -y mqtt-explorer &
 choco install -y chocolateygui &
 choco install -y cura &
+set UCS=https://raw.githubusercontent.com/iccasetti/scripts/main/files/cura.exe
+curl -o %temp%\cura.exe %UCS% && %temp%\cura.exe -y
+move %temp%\cura c:\users\default\appdata\roaming
 REM winget install -h --disable-interactivity "Chimpa Agent"
 REM admin@chimpa.private -> https://cloud.chimpa.eu/iccasetti/api/latest/mdm/windows/discovery_windows
 ECHO %USERNAME% > %TEMP%\WEB-BOOT.LOG
