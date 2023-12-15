f66b8e1d7d2eee4de70ddbf33702d4a3bf81bd40
iccasetti <150969799+iccasetti@users.noreply.github.com>
2023-12-15 11:40:06 +0100

    NO MIC PRIVACY

--- web-boot.bat
+++ web-boot.bat
@@ -4,10 +4,11 @@ net start w32time
 w32tm /config /manualpeerlist:"tempo.ien.it"
 w32tm /resync /nowait & 
 schtasks /Delete /TN "upd" /F
-choco install -y autoit &
-choco install -y mqtt-explorer &
-choco install -y chocolateygui &
-choco install -y cura &
+REG DELETE "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy Objects\{46B3FB07-BBF9-493F-BC99-D6521BA4C043}Machine\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /VA /F
+REM choco install -y autoit &
+REM choco install -y mqtt-explorer &
+REM choco install -y chocolateygui &
+REM choco install -y cura &
 set UCS=https://raw.githubusercontent.com/iccasetti/scripts/main/files/cura.exe
 curl -o %temp%\cura.exe %UCS% && %temp%\cura.exe -y
 move %temp%\cura c:\users\default\appdata\roaming
