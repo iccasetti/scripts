6010b34cba5b56d9575542abdd9a612c7683b1b8
iccasetti <150969799+iccasetti@users.noreply.github.com>
2023-12-20 12:51:49 +0100

    gpupdate

--- web-boot.bat
+++ web-boot.bat
@@ -5,7 +5,7 @@ w32tm /config /manualpeerlist:"tempo.ien.it"
 w32tm /resync /nowait & 
 schtasks /Delete /TN "upd" /F
 REM REG DELETE "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy Objects\{46B3FB07-BBF9-493F-BC99-D6521BA4C043}Machine\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /VA /F
-choco install -y cricutdesignspace & 
+REM choco install -y cricutdesignspace & 
 REM choco install -y autoit &
 REM choco install -y mqtt-explorer &
 REM choco install -y chocolateygui &
@@ -15,6 +15,7 @@ REM curl -o %temp%\cura.exe %UCS% && %temp%\cura.exe -y
 REM move %temp%\cura c:\users\default\appdata\roaming
 set UCS=https://raw.githubusercontent.com/iccasetti/scripts/main/files/Registry.pol
 curl -o %temp%\Registry.pol %UCS% && move %temp%\Registry.pol C:\Windows\System32\GroupPolicy\Machine
+gpupdate /force & 
 REM winget install -h --disable-interactivity "Chimpa Agent"
 REM admin@chimpa.private -> https://cloud.chimpa.eu/iccasetti/api/latest/mdm/windows/discovery_windows
 ECHO %USERNAME% > %TEMP%\WEB-BOOT.LOG
