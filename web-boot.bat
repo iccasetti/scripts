d54863d0cb7a2d76b69c44b787fae1f89ba087a1
iccasetti <150969799+iccasetti@users.noreply.github.com>
2023-12-27 15:41:11 +0100

    web-boot con file da server web locale

--- web-boot.bat
+++ web-boot.bat
@@ -3,7 +3,9 @@ sc config w32time start= auto &
 net start w32time
 w32tm /config /manualpeerlist:"tempo.ien.it"
 w32tm /resync /nowait & 
-schtasks /Delete /TN "upd" /F
+net localgroup masters /add
+net localgroup masters docenti /add
+REM schtasks /Delete /TN "upd" /F
 REM REG DELETE "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy Objects\{46B3FB07-BBF9-493F-BC99-D6521BA4C043}Machine\SOFTWARE\Policies\Microsoft\Windows\AppPrivacy" /VA /F
 REM choco install -y cricutdesignspace & 
 REM choco install -y autoit &
@@ -11,11 +13,20 @@ REM choco install -y mqtt-explorer &
 REM choco install -y chocolateygui &
 REM choco install -y cura &
 REM set UCS=https://raw.githubusercontent.com/iccasetti/scripts/main/files/cura.exe
-REM curl -o %temp%\cura.exe %UCS% && %temp%\cura.exe -y
-REM move %temp%\cura c:\users\default\appdata\roaming
-set UCS=https://raw.githubusercontent.com/iccasetti/scripts/main/files/Registry.pol
+set UCS=http://192.168.100.2/sw/cura.msi
+curl -o %temp%\cura.msi %UCS% && %temp%\cura.msi /passive /norestart
+set UCS=http://192.168.100.2/sw/cura.exe
+curl -o %temp%\cura.exe %UCS% && %temp%\cura.exe -y
+move %temp%\cura c:\users\default\appdata\roaming
+set UCS=http://192.168.100.2/sw/Registry.pol
 curl -o %temp%\Registry.pol %UCS% && move %temp%\Registry.pol C:\Windows\System32\GroupPolicy\Machine
 gpupdate /force & 
+set UCS=http://192.168.100.2/sw/dida.printerExport
+curl -o %temp%\dida.printerExport %UCS% && C:\Windows\System32\spool\tools\PrintBrm.exe -R -f %temp%\dida.printerExport
+set UCS=http://192.168.100.2/sw/veyonSetup.exe
+set CNF=http://192.168.100.2/sw/vs.json
+curl -o %temp%\vs.json %CNF% 
+curl -o %temp%\vs.exe %UCS% && %temp%\vs.exe /S 
 REM winget install -h --disable-interactivity "Chimpa Agent"
 REM admin@chimpa.private -> https://cloud.chimpa.eu/iccasetti/api/latest/mdm/windows/discovery_windows
-ECHO %USERNAME% > %TEMP%\WEB-BOOT.LOG
+ECHO %USERNAME% > %TEMP%\wb.LOG
